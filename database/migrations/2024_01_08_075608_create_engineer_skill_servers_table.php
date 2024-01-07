46f4a8dae134b2837426e82c251259cd6dc79bf8
TakumiYoshida <yoshida.fm.0626@gmail.com>
2024-01-08 08:27:47 +0900

    エンジニアの保有しているスキルのテーブルを追加

---
+++ database/migrations/2024_01_08_075608_create_engineer_skill_servers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('engineer_skill_servers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+            $table->string('name', 255);
+            $table->unsignedInteger('experience_months')->length(4);
+            $table->boolean('auto_renew_flag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('engineer_skill_servers');
+    }
+};
