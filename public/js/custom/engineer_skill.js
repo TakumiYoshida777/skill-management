2cbc468aa3d6f64e86121e5fff740356c1bf8c4b
TakumiYoshida <yoshida.fm.0626@gmail.com>
2024-01-14 16:29:23 +0900

    JSの内容をクリア

--- public/js/custom/engineer_skill.js
+++ public/js/custom/engineer_skill.js
@@ -1,19 +1,2 @@
 'use strict';
 
-$(document).ready(function() {
-    var formData = $('#myForm').serializeArray();
-    // debugger
-    $.ajax({
-        type: 'POST',
-        url: '{{ src("engineer_skill.js", $user->id) }}',
-        data: formData,
-        success: function(response) {
-            // 成功時の処理
-            console.log(response);
-        },
-        error: function(error) {
-            // エラー時の処理
-            console.log(error);
-        }
-    });
-});
